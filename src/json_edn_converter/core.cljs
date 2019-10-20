5926936aa46c3c94b0e02d24e10551521c908478
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2019-10-20 12:27:39 -0500

    Made basic textareas and wip on converting json to edn

--- src/json_edn_converter/core.cljs
+++ src/json_edn_converter/core.cljs
@@ -5,8 +5,24 @@
 ;; -------------------------
 ;; Views
 
+(defn json-to-edn [json-text]
+  (try
+    (let [json-object (js/JSON.parse json-text)]
+      (prn json-object)
+      (prn (js->clj json-object)))
+    (catch js/Object e
+      (prn e))))
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div [:h2 "JSON <-> EDN Converter"]]
+  [:div {:style {:display "flex"}}
+   [:textarea {:style {:flex 1}
+               :rows 10
+               :on-input (fn [event]
+                           (prn (.-value (.-target event)))
+                           (json-to-edn (.-value (.-target event))))}]
+   [:textarea {:style {:flex 1}
+               :rows 10}]])
 
 ;; -------------------------
 ;; Initialize app
